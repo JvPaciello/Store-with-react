19de5efad4af58295770047eab8796c4eb7f7e53
João Victor de Aquino Netto Paciello <joaovictorpaciello@gmail.com>
2024-10-24 14:15:50 -0300

    First commit

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <Link to="/" className="logo">
+        Store
+      </Link>
+      <nav className="navbar">
+        <Link to="/">Register Products</Link>
+        <Link to="/">Login / Sign Up</Link>
+      </nav>
+    </header>
+  );
+}
+export default Header;
