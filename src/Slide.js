40bd763abddc48221cd4bbeab77a72ca57eefcf5
João Victor de Aquino Netto Paciello <joaovictorpaciello@gmail.com>
2024-10-24 16:03:56 -0300

    Added an guitar store API (credits: https://github.com/VWRoli/guitar-store-api)

---
+++ src/Slide.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Slide(){
+    return(
+        <div className="container">
+            <div className="images">
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+            </div>
+        </div>
+    );
+}
+export default Slide;
