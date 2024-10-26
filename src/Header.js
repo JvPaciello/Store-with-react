b602d9acbf1b1f61b1f093111f62b28dedb364e3
João Victor de Aquino Netto Paciello <joaovictorpaciello@gmail.com>
2024-10-26 15:48:59 -0300

    Responsive header

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <header className="header">
       <Link to="/" className="logo">
-        Guitar info center
+        Guitar Info Center
       </Link>
-      <nav className="navbar">
+      <div className="menu-icon" onClick={toggleMenu}>
+        ☰
+      </div>
+      <nav className={`navbar ${menuActive ? "active" : ""}`}>
         <Link to="/">Register Products</Link>
         <Link to="/">Login / Sign Up</Link>
       </nav>
     </header>
   );
 }
+
 export default Header;
